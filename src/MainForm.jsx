3162981c8db44e671e1f81e9f10132e242c1422c
auritiwdesign <juancamilo.auriti@gmail.com>
2024-02-19 05:27:44 +0100

    Create Form
    Create form and implement useState Hook

--- src/MainForm.jsx
+++ src/MainForm.jsx
@@ -1,6 +1,28 @@
+import './Styles.css'
+import SignUp from "./SignUp.jsx";
+import {useState} from "react";
+
 const MainForm = () => {
+
+    const [step, setStep] = useState(0);
+
     return (
-        <div>MainForm</div>
+        <>
+            <div>
+                <div className="progress-bar">
+                    <div className="progress"></div>
+                </div>
+                <div className="container">
+                    <div>
+                        <SignUp/>
+                    </div>
+                    <div className="btn-container">
+                        <button className="btn" onClick={() => setStep(step -1)}>Prev</button>
+                        <button className="btn" onClick={() => setStep(step + 1)}>Next</button>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 export default MainForm
--- src/SignUp.jsx
+++ src/SignUp.jsx
@@ -1,6 +1,15 @@
+import './Styles.css'
 const SignUp = () => {
     return (
-        <div>SignUp</div>
+        <div className="form">
+            <h2>Signup Details</h2>
+            <label>Username</label>
+            <input type="text"/>
+            <label>Password</label>
+            <input type="password"/>
+            <label>Confirm Password</label>
+            <input type="password"/>
+        </div>
     )
 }
 export default SignUp
